1a71ca04e3afe9b149d6255422963a54177d6016
Alexander Rojas <alexander.rojas@gmail.com>
2022-04-13 10:38:11 +0200

    Better structure for lexer tests

--- golang/lexer/lexer_test.go
+++ golang/lexer/lexer_test.go
@@ -7,17 +7,26 @@ import (
 
 var lexerTests = []struct {
 	source string
+	want   []Token
 }{
-	{"&"},
+	{"&", []Token{
+		&Tok{tag: Tag('&')},
+	}},
 }
 
 func TestLexer(t *testing.T) {
 	for _, test := range lexerTests {
 		s := strings.NewReader(test.source)
 		l := NewLexer(s)
-		_, err := l.Scan()
-		if err != nil {
-			t.Fatalf("Error scanning input '%s': %v", test.source, err)
+		for _, tk := range test.want {
+			tok, err := l.Scan()
+			if err != nil {
+				t.Fatalf("Error scanning input '%s': %v", test.source, err)
+			}
+			if tok.String() != tk.String() || tok.Tag() != tk.Tag() {
+				t.Fatalf("Expected token {tag: %d str: %s} does not match {tag: %d str: %s}",
+					tk.Tag(), tk.String(), tok.Tag(), tok.String())
+			}
 		}
 	}
 }
