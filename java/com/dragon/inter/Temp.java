6e7f9af76fc10f7099fd74fbabdc4344b90830ed
Alexander Rojas <alexander.rojas@gmail.com>
2023-02-13 14:20:22 +0100

    Static variables are atomic

--- java/com/dragon/inter/Temp.java
+++ java/com/dragon/inter/Temp.java
@@ -1,16 +1,20 @@
 package com.dragon.inter;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import com.dragon.lexer.Word;
 import com.dragon.symbols.Type;
 
 public class Temp extends Expr {
-  private static int count = 0;
+  private static final AtomicInteger count = new AtomicInteger(0);
 
   private int number = 0;
 
   public Temp(Type t) {
     super(Word.temp, t);
-    number = ++count;
+    number = count.addAndGet(1);
+    var next = number+1;
+    count.compareAndSet(number, next);
   }
 
   @Override
@@ -19,6 +23,6 @@ public class Temp extends Expr {
   }
 
   static public void resetTempCount() {
-    count = 0;
+    count.getAndSet(0);
   }
 }
