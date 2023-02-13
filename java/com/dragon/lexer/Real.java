d12db78a65c93bbd9c7ec7f54874ec9c273ab15b
Alexander Rojas <alexander.rojas@gmail.com>
2023-02-13 17:20:31 +0100

    Adds Java unit tests

--- java/com/dragon/lexer/Real.java
+++ java/com/dragon/lexer/Real.java
@@ -1,18 +1,18 @@
 package com.dragon.lexer;
 
 public class Real extends Token {
-  private final float value;
+  private final double value;
 
-  public Real(float v) {
+  public Real(double v) {
     super(Tag.REAL);
     value = v;
   }
 
-  public float getValue() {
+  public double getValue() {
     return value;
   }
 
   public String toString() {
-    return "" + value;
+    return String.format("%.3f", value);
   }
 }
---
+++ java/com/dragon/main/FrontEndTests.java
@@ -0,0 +1,14 @@
+package com.dragon.main;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import com.dragon.lexer.LexerTests;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+  LexerTests.class
+})
+public class FrontEndTests {
+
+}
